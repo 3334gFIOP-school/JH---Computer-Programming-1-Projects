4f85444848c711ccef3850e9e0433f1a7e472fde
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-09-02 21:34:26 -0600

    I did the next project that it needed for computer programming, now im asking who the person is

--- First_Project/main.py
+++ First_Project/main.py
@@ -2,5 +2,12 @@
 
 import random
 
+x=0
+
 num = random.randint(1,20)
 print("this is just a test", num)
+
+while x < 100:
+    num = random.randint(1,20)
+    print(num)
+    x = x+1
---
+++ whoAreYou/main.py
@@ -0,0 +1,7 @@
+# Jackson Hauley - Who are you?
+
+name = input("What is your name?: \n")
+age = input("What is your age?: \n")
+color = input("What is your favorite color?: \n")
+
+print("Your name is", name,", your favorite color is", color,", and you are", age,"years old.")
