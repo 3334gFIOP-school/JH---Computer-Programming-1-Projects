dc114d95922d079d3af7d310cc82cd819d6cd7b5
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-09-03 19:34:57 -0600

    I made a celsius to fahrenheit converter in python code for computer programming class

---
+++ Celsius to Fahrenheit/main.py
@@ -0,0 +1,11 @@
+# Jackson Hauley - Celsius to Fahrenheit
+
+C = input("Enter temperature in Celsius: ")
+
+C = int(C)
+
+F = (C * 9/5) + 3220
+
+F = str(F)
+
+print("When it is",C,"degrees Celsius it is",F,"degrees Fahrenheit")
