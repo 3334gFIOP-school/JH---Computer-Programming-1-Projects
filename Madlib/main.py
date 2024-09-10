5ccbd19af90e30551c3dfaf78f35c534e0cecf45
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-09-09 21:07:43 -0600

    I made a madlib for my CS class

---
+++ Madlib/main.py
@@ -0,0 +1,19 @@
+# Jackson Hauley Madlib Proficiency Test
+
+# Madlib Inputs
+verb = input("What is a random verb?: ")
+verb2 = input("What is another random verb?: ")
+noun = input("What is a random plural noun?: ")
+noun2 = input("What is another random plural noun?: ")
+adjective = input("What is a random adjective?: ")
+adjective2 = input("What is another random adjective?: ")
+
+# Printing the Madlib
+print("""Once upon a time, there was a child, who really loved to
+""",verb,"""all the time, and one day, (s)he want to the
+store to buy some""",noun,"and then (s)he ate the",noun,"""
+and really loved it, so much that (s)he wanted to""",verb2+"."""" One day
+when (s)he was eating""",noun,"""an old man who was very""",adjective,"""
+came up to the child, and said, 'Hey do you want some""",noun2,"""?
+and the child said that he was""",adjective2,"then",verb,"""away
+never to be seen again.""")
