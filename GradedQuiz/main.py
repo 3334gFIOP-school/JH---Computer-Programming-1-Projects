0f460b5c1bebe0b236e2c176bdf011441fec147a
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-09-17 09:20:03 -0600

    hi there

---
+++ GradedQuiz/main.py
@@ -0,0 +1,14 @@
+# Jackson Hauley - Graded Quiz proficiency test
+
+# Score declaring
+score = 0
+
+# Questions
+answer1 = int(input("What is 1 + 1?: "))
+if answer1 == 2:
+    score += 1
+
+answer2 = str(input("What comes after D in the alphabet?: "))
+if answer2 == "E":
+    score += 1
+
