67f903aed611d393effa5c50860b526790ce74f6
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-09-24 17:38:53 -0600

    piglatin converter

--- PigLatinConverter/main.py
+++ PigLatinConverter/main.py
@@ -1,20 +1,22 @@
 # Jackson Hauley - Pig Latin Converter
 
 # Checking for vowels
-vowels = ["a","e","i","o","u"]
+vowels = ["a", "e", "i", "o", "u"]
 
 # Function Created
 def convert(pig):
-    output = ""
-    piglist = list(pig)
-    if piglist[0] == vowels:
-        testing = piglist[0]
-        piglist.remove[0]
-        print(piglist)
-        piglist.append(testing)
-    output = "".join(piglist)
-    output = output + "ay"
-    return(output)
+    piglist = list(pig.lower())
+    if piglist[0] in vowels:
+        output = pig + "yay"
+    else:
+        for x in range(len(piglist)):
+            if piglist[x] in vowels:
+                output = "".join(piglist[x:]) + "".join(piglist[:x]) + "ay"
+                break
+        else:
+            output = pig + "ay"
+    
+    return output
 
 # Testing
 print(convert(input("What do you want to convert?: ")))
