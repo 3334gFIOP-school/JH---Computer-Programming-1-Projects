d305ee9495a72fcfc067bd891e2cc38cf3b85cf4
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-09-25 09:52:52 -0600

    Broken

--- SecretCypher/main.py
+++ SecretCypher/main.py
@@ -2,17 +2,23 @@
 
 # List of all the letters
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alphabetString = "abcdefghijklmnopqrstuvwxyz"
 
 # Creating function
 def encode(secret):
     secretlist = list(secret)
     for x in range(7):
-        secretlist.append(secretlist[0])
+        print(secretlist) # temporary 
+        firstLetter = secretlist[0]
+        # THIS IS THE MOST BROKEN CRAP THAT EVER EXISTED
+        print(firstLetter) # temporary
+        position = secretlist.index(firstLetter)
         secretlist.pop(0)
-    return("".join(secretlist))
-
+        secretlist.append(position)
+    word2 = "".join(secretlist)
+    return(word2)
 
 
 # Testing function
-word = input('What word do you want to decode?: ')
+word = input('What word do you want to encode?: ')
 print(encode(word))
