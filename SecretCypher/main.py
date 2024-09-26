f79aaa4b02959546810e7fadd21de9f7456e2460
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-09-25 22:47:56 -0600

    Full caesar cypher encodes/decoder

--- SecretCypher/main.py
+++ SecretCypher/main.py
@@ -1,24 +1,49 @@
 # Jackson Hauley - Secret Cypher
 
-# List of all the letters
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+# Declaring alphabet string
 alphabetString = "abcdefghijklmnopqrstuvwxyz"
 
-# Creating function
+# Defining function
 def encode(secret):
+    secret = secret.lower()
     secretlist = list(secret)
-    for x in range(7):
-        print(secretlist) # temporary 
-        firstLetter = secretlist[0]
-        # THIS IS THE MOST BROKEN CRAP THAT EVER EXISTED
-        print(firstLetter) # temporary
-        position = secretlist.index(firstLetter)
-        secretlist.pop(0)
-        secretlist.append(position)
-    word2 = "".join(secretlist)
-    return(word2)
+    encoded = [""]
+    for firstLetter in secretlist:
+        if firstLetter in alphabetString:
+            position = alphabetString.index(firstLetter)
+            position2 = (position + decodeAmount) % 26
+            encoded.append(alphabetString[position2])
+        else:
+            encoded.append(firstLetter)
+    word2 = "".join(encoded)
+    return word2
 
+def decode(secret2):
+    secret2 = secret2.lower()
+    secretlist2 = list(secret2)
+    encoded2 = [""]
+    for firstLetter in secretlist2:
+        if firstLetter in alphabetString:
+            position3 = alphabetString.index(firstLetter)
+            position4 = (position3 - decodeAmount) % 26
+            encoded2.append(alphabetString[position4])
+        else:
+            encoded2.append(firstLetter)
+    word2 = "".join(encoded2)
+    return word2
 
-# Testing function
-word = input('What word do you want to encode?: ')
-print(encode(word))
+def ask():
+    decide = input("Do you want to encode or decode?: ")
+    if "encode" in decide:
+        sentence = input("What sentence do you want to encode?: ")
+        print("Encoded sentence:", encode(sentence))
+    elif "decode" in decide:
+        sentence = input("What sentence do you want to decode?: ")
+        print("Decoded sentence:", decode(sentence))
+    else:
+        print('That is not a choice! Type either "encode" or "decode"!') 
+        ask()
+
+# Starting Code
+decodeAmount = int(input("How many letters do you want to decode/encode by? "))
+ask()
