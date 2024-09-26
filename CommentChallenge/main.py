c936ed955e04e7c9bcbf80837e7241eff1ed9deb
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-09-25 23:16:20 -0600

    comment challenge!

---
+++ CommentChallenge/main.py
@@ -0,0 +1,6 @@
+# Jackson Hauley - Comment Challenge
+
+
+# This is code that asks for someones name then prints "Hello, name"
+name = input("What is your name?: ") # This is the input name
+print("Hello,",name) # This is printing the sentence
