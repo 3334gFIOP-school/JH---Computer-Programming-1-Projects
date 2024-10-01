c088278d42823d67acaf014ae1f963d352ba0204
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-10-01 09:46:41 -0600

    fixed area calculator

--- AreaCalculator/main.py
+++ AreaCalculator/main.py
@@ -6,7 +6,7 @@ def calculateTriangle(height,base): return base*height*1/2
 def calculateCircle(radius): return math.pi*radius**2
 def calculateTrapezoid(base1,base2,height): return (base1+base2)/2*height
 def ask():
-    print("What do you need to calculate?\n1. Area of a Square\n2. Area of a Rectangle\n3. Area of a Triangle\n4. Area of a Circle\n5. Area of a Trapezoid")
+    print("What do you need to calculate?\n1. Area of a Square\n2. Area of a Rectangle\n3. Area of a Triangle\n4. Area of a Circle\n5. Area of a Trapezoid\n(wrong int will default to trapezoid)")
     selection = int(input("Select which number: "))
     if selection == 1: return calculateSquare(int(input("What is the side length of the square?: ")))
     elif selection == 2: return calculateRectangle(int(input("What is the heigth of the rectangle?: ")),int(input("What is the width of the rectangle?: ")))
