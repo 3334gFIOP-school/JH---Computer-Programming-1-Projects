6938f11c938565fd8814282aca86926bf3d1e0e7
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2024-10-03 09:25:32 -0600

    List of classes!

---
+++ ListOfClasses/main.py
@@ -0,0 +1,6 @@
+# Jackson Hauley - List of Classes
+
+classes = ["CS","MATH","ENGLISH","ADVISORY","DIGITAL MEDIA","LEADERSHIP","CHEMISTRY"]
+print(classes)
+for x in range(len(classes)):
+    print(classes[x])
